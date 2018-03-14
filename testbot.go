4314c184e2c6923b1eeae47f55bfd50a8595a47d
Joshua Kaplan <yhkaplan@gmail.com>
2018-03-14 09:52:57 +0900

    implented respond func

--- testbot.go
+++ testbot.go
@@ -50,3 +50,30 @@ Loop:
 		}
 	}
 }
+
+func respond(rtm *slack.RTM, msg *slack.MessageEvent, prefix string) {
+	var response string // I guess the zero value is used, so this syntax is used instead
+	text := msg.Text
+	text = strings.TrimPrefix(text, prefix)
+	text = strings.TrimSpace(text)
+	text = strings.ToLower(text)
+
+	acceptedGreetings := map[string]bool{
+		"what's up?": true,
+		"hey!":       true,
+		"yo":         true,
+	}
+	acceptedHowAreYou := map[string]bool{
+		"how's it going?": true,
+		"how are ya?":     true,
+		"feeling okay?":   true,
+	}
+
+	if acceptedGreetings[text] { //Uses the fact that the zero value of Bool is false?
+		response = "What's up buddy?"
+		rtm.SendMessage(rtm.NewOutgoingMessage(response, msg.Channel))
+	} else if acceptedHowAreYou[text] {
+		response = "Good, how are you?"
+		rtm.SendMessage(rtm.NewOutgoingMessage(response, msg.Channel))
+	}
+}
